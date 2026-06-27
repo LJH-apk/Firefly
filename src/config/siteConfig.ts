9b124b42fe43f0a4df804aa83a14e42f6b9e88e3
刘佳航 <liu18701059325@qq.com>
2026-06-27 20:44:12 +0800

    fix:更改了图片头像

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -65,7 +65,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/firefly.png",
+			value: "assets/images/avatar.avif",
 			alt: "Firefly",
 		},
 		// 导航栏标题
